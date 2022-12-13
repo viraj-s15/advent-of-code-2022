a4ed9ce8ddfd4ee2b53f2f017d21b93c19381d2c
viraj-s15 <veer15102003@gmail.com>
2022-12-13 23:02:23 +0530

    Day 1

---
+++ Day 1/day1.py
@@ -0,0 +1,25 @@
+file_path = '/home/veer/Documents/advent-of-code/Day 1/input.in'
+
+data_set = open(file_path, "r").read().split("\n\n")
+maximum_cal = 0
+sorted_array = []
+
+
+
+for i in data_set:
+    maximum_cal = max(maximum_cal,sum([int(r) for r in i.splitlines()]))
+    sorted_array.append(sum(int(j) for j in i.splitlines()))
+    
+sorted_array = sorted(sorted_array)
+    
+print("Part 1 solution:", maximum_cal)
+print("Part 2 solution:", sum(sorted_array[-3:]))
+
+
+    
+    
+    
+    
+    
+    
+    
